15f6d0be65196000c49eadacca64c4c8e4219f39
185721161@qq.com <185721161@qq.com>
2021-02-08 12:42:47 +0800

    第一版

---
+++ src/main/java/vicky/Test/AdaBoostTest.java
@@ -0,0 +1,20 @@
+package vicky.Test;
+
+import vicky.ML.AdaBoost.AdaBoostTool;
+
+/**
+ * AdaBoost提升算法调用类
+ * @author lyq
+ *
+ */
+public class AdaBoostTest {
+
+    public static void main(String[] agrs){
+        String filePath = "C:\\Users\\sdc\\Desktop\\DataMiningAlgorithm-master\\BaggingAndBoosting\\DataMining_AdaBoost\\input.txt";
+        //误差率阈值
+        double errorValue = 0.2;
+
+        AdaBoostTool tool = new AdaBoostTool(filePath, errorValue);
+        tool.adaBoostClassify();
+    }
+}
