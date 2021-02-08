984f1a684e8ce043c98a24cb59664ad69ade23c5
185721161@qq.com <185721161@qq.com>
2021-02-08 11:34:38 +0800

    第一版

--- src/main/java/vicky/DS/Sort/HeapSort.java
+++ src/main/java/vicky/DS/Sort/HeapSort.java
@@ -39,11 +39,11 @@ public class HeapSort {
             return false;
     }
 
-    public static void main(String [] args){
+   /* public static void main(String [] args){
         int array[] = {3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48};
          new HeapSort(array);
          for(int i=0; i< array.length;i++){
             System.out.println(array[i]);
          }
-    }
+    }*/
 }
